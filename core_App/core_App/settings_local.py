29d4faf880e57d5b51f7699c7f7618a86bd16573
bergaeduardo <bergaeduardo@gmail.com>
2025-06-24 11:31:23 -0300

    Aumentar tiempo de sesion

--- core_App/core_App/settings_local.py
+++ core_App/core_App/settings_local.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from datetime import timedelta # Importar timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,7 +63,7 @@ DATABASES = {
     },
     'sqlserver': {
         'ENGINE': 'mssql',
-        'NAME': 'Empresa_Ejemplo',
+        'NAME': 'LAKER_SA',
         'USER': 'sa',
         'PASSWORD': 'Axoft1988',
         'HOST': 'SERVIDOR',
@@ -101,6 +102,40 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    # Configuración para extender la duración del token JWT
+    'SIMPLE_JWT': {
+        'ACCESS_TOKEN_LIFETIME': timedelta(days=1), # Token de acceso válido por 24 horas
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=7), # Opcional: Token de refresco válido por 7 días
+        'ROTATE_REFRESH_TOKENS': False,
+        'BLACKLIST_AFTER_ROTATION': False,
+        'UPDATE_LAST_LOGIN': False,
+
+        'ALGORITHM': 'HS256',
+        'SIGNING_KEY': SECRET_KEY,
+        'VERIFYING_KEY': '',
+        'AUDIENCE': None,
+        'ISSUER': None,
+        'JSON_ENCODER': None,
+        'JWK_URL': None,
+        'LEEWAY': 0,
+
+        'AUTH_HEADER_TYPES': ('Bearer',),
+        'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+        'USER_ID_FIELD': 'id',
+        'USER_ID_CLAIM': 'user_id',
+        'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+        'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+        'TOKEN_TYPE_CLAIM': 'token_type',
+        'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+        'JTI_CLAIM': 'jti',
+
+        'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+        'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+        'SLIDING_TOKEN_OBTAIN_PAIR_CLASS': 'rest_framework_simplejwt.tokens.SlidingToken',
+        'SLIDING_TOKEN_REFRESH_PAIR_CLASS': 'rest_framework_simplejwt.tokens.SlidingToken',
+    }
 }
 
 DATABASE_ROUTERS = ['core_App.db_routers.DatabaseRouter']
@@ -113,9 +148,9 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # Cierra la sesión al cerrar el navegador
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Para que las sesiones duren 1 Hora:
-SESSION_COOKIE_AGE = 1 * 60 * 60
+SESSION_COOKIE_AGE = 12 * 60 * 60
 
-""" 
+"""
 La expresión 12 * 60 * 60 se refiere a la cantidad de segundos que representa la duración de la sesión.
 
 Aquí te explico la lógica detrás de esta expresión:
@@ -125,4 +160,5 @@ Aquí te explico la lógica detrás de esta expresión:
 12 horas son... ¡12 horas!
 Entonces, 12 * 60 * 60 es igual a 12 horas x 60 minutos/hora x 60 segundos/minuto = 43200 segundos
 
-Por lo tanto, SESSION_COOKIE_AGE = 12 * 60 * 60 representa una duración de sesión de 12 horas. """
+Por lo tanto, SESSION_COOKIE_AGE = 12 * 60 * 60 representa una duración de sesión de 12 horas.
+"""
--- core_App/core_App/settings_production.py
+++ core_App/core_App/settings_production.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+from datetime import timedelta # Importar timedelta
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.messages',
             ],
         },
     },
@@ -119,12 +120,46 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # Cierra la sesión al cerrar el navegador
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Para que las sesiones duren 1 día:
-SESSION_COOKIE_AGE = 1 * 60 * 60
+SESSION_COOKIE_AGE = 12 * 60 * 60
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    # Configuración para extender la duración del token JWT
+    'SIMPLE_JWT': {
+        'ACCESS_TOKEN_LIFETIME': timedelta(days=1), # Token de acceso válido por 24 horas
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=7), # Opcional: Token de refresco válido por 7 días
+        'ROTATE_REFRESH_TOKENS': False,
+        'BLACKLIST_AFTER_ROTATION': False,
+        'UPDATE_LAST_LOGIN': False,
+
+        'ALGORITHM': 'HS256',
+        'SIGNING_KEY': SECRET_KEY,
+        'VERIFYING_KEY': '',
+        'AUDIENCE': None,
+        'ISSUER': None,
+        'JSON_ENCODER': None,
+        'JWK_URL': None,
+        'LEEWAY': 0,
+
+        'AUTH_HEADER_TYPES': ('Bearer',),
+        'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+        'USER_ID_FIELD': 'id',
+        'USER_ID_CLAIM': 'user_id',
+        'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+        'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+        'TOKEN_TYPE_CLAIM': 'token_type',
+        'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+        'JTI_CLAIM': 'jti',
+
+        'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+        'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+        'SLIDING_TOKEN_OBTAIN_PAIR_CLASS': 'rest_framework_simplejwt.tokens.SlidingToken',
+        'SLIDING_TOKEN_REFRESH_PAIR_CLASS': 'rest_framework_simplejwt.tokens.SlidingToken',
+    }
 }
 
 DATABASE_ROUTERS = ['core_App.db_routers.DatabaseRouter']
