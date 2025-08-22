63600ae954fd4f8670364e9d6ac5c31004102ba2
bergaeduardo <bergaeduardo@gmail.com>
2025-08-22 16:23:39 -0300

    Restricción de acceso por IP para la aplicación Administracion

--- core_App/Administracion/middleware.py
+++ core_App/Administracion/middleware.py
@@ -1,5 +1,8 @@
 from django.utils.deprecation import MiddlewareMixin
 from .models import Proveedor
+from django.conf import settings
+from django.http import HttpResponseForbidden
+from ipaddress import ip_address, ip_network
 
 class ProveedorContextMiddleware(MiddlewareMixin):
   def process_request(self, request):
@@ -13,3 +16,31 @@ class ProveedorContextMiddleware(MiddlewareMixin):
         request.proveedor = Proveedor.objects.select_related('username_django').get(pk=pid)
       except Proveedor.DoesNotExist:
         request.session.pop('proveedor_id', None)
+
+
+
+class IPWhitelistMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.allowed_networks = [ip_network(str(ip), strict=False) for ip in settings.ALLOWED_ADMIN_IPS]
+
+    def __call__(self, request):
+        if request.path.startswith('/administracion/'):
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0].strip()
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+
+            if ip:
+                print(f"Request from IP: {ip} to {request.path}") # Log the IP
+                client_ip = ip_address(ip)
+                is_allowed = any(client_ip in net for net in self.allowed_networks)
+
+                if not is_allowed:
+                    return HttpResponseForbidden("Acceso denegado. Su dirección IP no está permitida para acceder a esta sección.")
+            else:
+                return HttpResponseForbidden("Acceso denegado. No se pudo determinar su dirección IP.")
+
+        response = self.get_response(request)
+        return response
